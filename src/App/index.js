56253e75bbc8cd7a2e983be0cb9550c94c0198c6
Ryan Hikel <hikelryan@gmail.com>
2018-06-21 09:16:11 -0400

    added base components and routes

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
 import "./style.css";
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Landing from "../Landing";
+import User from "../User";
+import ShowDoc from "../ShowDoc";
 class App extends Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return (
+      <Router>
+        <div className='App'>
+          <Link to='/'>Landing</Link>
+          <Link to='/doctor/:id'>Find</Link>
+          <Link to='/user/:id'>Profile</Link>
+          <Route path='/' exact component={Landing} />
+          <Route path='/doctor/:id' exact component={ShowDoc} />
+          <Route path='/user/:id' exact component={User} />
+        </div>
+      </Router>
+    )
   }
 }
 
---
+++ src/Landing/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./style.css";
+class Landing extends Component {
+  render() {
+    return(
+    <div>
+      <h1>HI</h1>
+    </div>
+    )
+  }
+}
+
+export default Landing;
